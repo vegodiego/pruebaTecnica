19b971c3a567b28a1f56b5354ea459a4af9639e3
diego alejandro vélez <vegodiego@hotmail.com>
2021-09-03 18:32:13 -0500

    se crearon las entidades Construccion y Material

---
+++ src/pruebaTecnicaCoreVida/dominio/ciudadela/Construccion.java
@@ -0,0 +1,60 @@
+package pruebaTecnicaCoreVida.dominio.ciudadela;
+
+import pruebaTecnicaCoreVida.dominio.ciudadela.objetosDeValor.*;
+
+public class Construccion {
+    private static Integer ultimoId = -1;
+
+    private Integer id;
+    private Dias dias;
+    private Nombre nombre;
+    private CantidadAdobe cantidadAdobe;
+    private CantidadArena cantidadArena;
+    private CantidadCemento cantidadCemento;
+    private CantidadGrava cantidadGrava;
+    private CantidadMadera cantidadMadera;
+
+    public Construccion(Dias dias, Nombre nombre, CantidadAdobe cantidadAdobe, CantidadArena cantidadArena, CantidadCemento cantidadCemento, CantidadGrava cantidadGrava, CantidadMadera cantidadMadera) {
+        ultimoId++;
+        this.id = ultimoId ;
+        this.dias = dias;
+        this.nombre = nombre;
+        this.cantidadAdobe = cantidadAdobe;
+        this.cantidadArena = cantidadArena;
+        this.cantidadCemento = cantidadCemento;
+        this.cantidadGrava = cantidadGrava;
+        this.cantidadMadera = cantidadMadera;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Dias getDias() {
+        return dias;
+    }
+
+    public Nombre getNombre() {
+        return nombre;
+    }
+
+    public CantidadAdobe getCantidadAdobe() {
+        return cantidadAdobe;
+    }
+
+    public CantidadArena getCantidadArena() {
+        return cantidadArena;
+    }
+
+    public CantidadCemento getCantidadCemento() {
+        return cantidadCemento;
+    }
+
+    public CantidadGrava getCantidadGrava() {
+        return cantidadGrava;
+    }
+
+    public CantidadMadera getCantidadMadera() {
+        return cantidadMadera;
+    }
+}
---
+++ src/pruebaTecnicaCoreVida/dominio/ciudadela/Material.java
@@ -0,0 +1,35 @@
+package pruebaTecnicaCoreVida.dominio.ciudadela;
+
+import pruebaTecnicaCoreVida.dominio.ciudadela.objetosDeValor.Cantidad;
+import pruebaTecnicaCoreVida.dominio.ciudadela.objetosDeValor.Nombre;
+
+public class Material {
+    private static Integer ultimoId = -1;
+
+    private Integer id;
+    private Nombre nombre;
+    private Cantidad cantidad;
+
+    public Material(Nombre nombre, Cantidad cantidad) {
+        ultimoId++;
+        this.id = ultimoId ;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Nombre getNombre() {
+        return nombre;
+    }
+
+    public Cantidad getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Cantidad cantidad) {
+        this.cantidad = cantidad;
+    }
+}
