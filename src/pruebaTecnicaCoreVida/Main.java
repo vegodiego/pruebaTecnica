fab02c208f29a7706004a6f7990531d7eacadbe8
diego alejandro vélez <vegodiego@hotmail.com>
2021-09-03 18:38:23 -0500

    Se crearon los materiales, contrucciones y la ciudadela en el ejecutable

--- src/pruebaTecnicaCoreVida/Main.java
+++ src/pruebaTecnicaCoreVida/Main.java
@@ -1,7 +1,47 @@
 package pruebaTecnicaCoreVida;
 
+import pruebaTecnicaCoreVida.dominio.ciudadela.Ciudadela;
+import pruebaTecnicaCoreVida.dominio.ciudadela.Construccion;
+import pruebaTecnicaCoreVida.dominio.ciudadela.Material;
+import pruebaTecnicaCoreVida.dominio.ciudadela.objetosDeValor.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        //Creacion de la lista de construcciones
+        List<Construccion> construcciones = new ArrayList<>();
+        List<String> nombresDeLasConstrucciones = Arrays.asList("casa", "lago", "cancha de futbol", "edificio", "gimnasio");
+        List<List<Integer>> cantidadDeMaterialPorConstruccion = Arrays.asList(Arrays.asList(100,90,100,50,20),Arrays.asList(20,80,50,60,10), Arrays.asList(20,20,20,20,20), Arrays.asList(200,180,200,100,40), Arrays.asList(50,45,50,25,10));
+        List<Integer> diasPorConstruccion = Arrays.asList(3,2,1,6,2);
 
+        for (int i = 0; i < cantidadDeMaterialPorConstruccion.size() ; i++) {
+            Dias dias = new Dias(diasPorConstruccion.get(i));
+            Nombre nombre = new Nombre(nombresDeLasConstrucciones.get(i));
+            CantidadAdobe cantidadAdobe = new CantidadAdobe(cantidadDeMaterialPorConstruccion.get(i).get(0));
+            CantidadArena cantidadArena = new CantidadArena(cantidadDeMaterialPorConstruccion.get(i).get(1));
+            CantidadCemento cantidadCemento = new CantidadCemento(cantidadDeMaterialPorConstruccion.get(i).get(2));
+            CantidadGrava cantidadGrava = new CantidadGrava(cantidadDeMaterialPorConstruccion.get(i).get(3));
+            CantidadMadera cantidadMadera = new CantidadMadera(cantidadDeMaterialPorConstruccion.get(i).get(4));
+            Construccion construccion = new Construccion(dias, nombre, cantidadAdobe, cantidadArena, cantidadCemento, cantidadGrava, cantidadMadera);
+            construcciones.add(construccion);
+        }
+
+        //Creacion de la lista de materiales
+        List<Material> materiales = new ArrayList<>();
+        List<String> nombresDeLosMateriales = Arrays.asList("cemento", "grava", "arena", "madera", "adobe");
+        List<Integer> cantidadTotalDeLosMateriales = Arrays.asList(500, 500, 500, 500 ,500);
+
+        for (int i = 0; i < cantidadTotalDeLosMateriales.size() ; i++) {
+            Nombre nombre = new Nombre(nombresDeLosMateriales.get(i));
+            Cantidad cantidad = new Cantidad(cantidadTotalDeLosMateriales.get(i));
+            Material material = new Material(nombre, cantidad);
+            materiales.add(material);
+        }
+
+        //creacion de la ciudadela
+        Ciudadela ciudadela = new Ciudadela(construcciones, materiales);
     }
 }
